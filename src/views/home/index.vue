f8df1208ed8a5ec1e1470a00284baa68b0733dcf
xmx <212539204@qq.com>
2020-02-23 20:01:23 +0800

    导航栏封装

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,24 @@
 <template>
-    <div>
-        首页
+    <div id="home">
+        <nav-bar class="home-nav">
+            <div slot="center">
+                <span>购物街</span>
+            </div>
+        </nav-bar>
     </div>
 </template>
 <script>
+import NavBar from 'components/common/navbar/NavBar'
 export default {
-    name:'Home'
+    name:'Home',
+    components:{
+        NavBar
+    }
 }
 </script>    
-<style lang="">
-    
+<style>
+    .home-nav{
+        background-color:RGB(255,150,170);
+        color:#fff;
+    }
 </style>
